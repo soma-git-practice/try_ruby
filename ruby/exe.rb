0d15fb60a83d0aea79432609fb018cfe98a6afb8
soma <somofgit@gmail.com>
2024-11-29 08:42:49 +0900

    一つの属性のアップデートでやりたかったことの再現

--- ruby/exe.rb
+++ ruby/exe.rb
@@ -1,58 +1,49 @@
 require 'irb'
 require 'minitest/autorun'
 
-module Devise
-  def self.add_module(module_name)
-    Mapping.add_module module_name
+class User
+  attr_accessor :name, :age, :weight
+
+  def self.column_names
+    %w(name age weight)
   end
 
-  class Mapping
-    def self.add_module(module_name)
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{module_name}?
-          @moodules.include?(#{module_name})
-        end
-      METHOD
-    end
-
-    attr_reader :modules
-
-    def initialize
-      @modules = []
-    end
-
-    def modules=(value)
-      @modules += [*value]
-    end
+  def update(hash = {})
+    hash.keys.each{|k| send("#{k}=", hash[k])}
   end
 end
 
-class TestDevise < Minitest::Spec
-  after do
-    if Devise::Mapping.public_instance_methods.include?(:devise_hoge?)
-      Devise::Mapping.remove_method(:devise_hoge?)
+User.column_names.each do |column_name|
+  User.class_eval <<-METHOD, __FILE__, __LINE__ + 1
+    def update_#{column_name}(value)
+      update(#{column_name}: value)
     end
+  METHOD
+end
+
+class ModelReplicaTest < Minitest::Spec
+  it 'User.column_name' do
+    assert_equal %w(name age weight), User.column_names
   end
 
-  it 'Devise default' do
-    assert_equal false, Devise::Mapping.public_instance_methods.include?(:devise_hoge?)
+  it 'user.update' do
+    obj = User.new
+    obj.update(name: '太郎', age: 1000, weight: '60000kg')
+    assert_equal '太郎', obj.name
+    assert_equal 1000, obj.age
+    assert_equal '60000kg', obj.weight
   end
 
-  it 'Devise#add_module' do
-    Devise::Mapping.add_module :devise_hoge
-    assert_equal true, Devise::Mapping.public_instance_methods.include?(:devise_hoge?)
-  end
+  it 'single column update' do
+    obj = User.new
 
-  it '#Devise::Mapping default' do
-    obj = Devise::Mapping.new
-    obj.modules = %w(devise_hoge)
-    assert_equal false, obj.public_methods.include?(:devise_hoge?)
-  end
+    obj.update_name '田中'
+    assert_equal '田中', obj.name
 
-  it 'Devise::Mapping#add_module' do
-    Devise::Mapping.add_module :devise_hoge
-    obj = Devise::Mapping.new
-    obj.modules = %w(devise_hoge)
-    assert_equal true, obj.public_methods.include?(:devise_hoge?)
+    obj.update_age 50
+    assert_equal 50, obj.age
+
+    obj.update_weight '7000kg'
+    assert_equal '7000kg', obj.weight
   end
 end
