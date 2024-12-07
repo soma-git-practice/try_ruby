0f1af754b990ad9b3150db930fc83ca912c88783
soma <somofgit@gmail.com>
2024-12-08 08:44:56 +0900

    User.case_insensitive_keysの動作確認

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem 'irb'
 gem 'minitest', '~> 5.12', '>= 5.12.2'
+gem 'activesupport', '7.0.8'
--- ruby/exe.rb
+++ ruby/exe.rb
@@ -1,6 +1,7 @@
 require 'irb'
 require 'minitest/autorun'
-
+require 'active_support/concern'
+require 'active_support/core_ext/module/attribute_accessors'
 
 # ** 目標 **
 # 
@@ -8,46 +9,47 @@ require 'minitest/autorun'
 #  何をしているのか知りたいから読む。
 #  https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models/authenticatable.rb#L220-L223
 
+# https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise.rb#L89-L91
+module Devise
+  mattr_accessor :request_keys
+  @@request_keys = [:devise_value]
 
-# ActiveSupportから必要な箇所を抜粋
-# https://github.com/rails/rails/blob/31aec233a17a44cb4666aa074243f0961798ed46/activesupport/lib/active_support/concern.rb#L125-L140
-module ActiveSupport
-  module Concern
-    def append_features(base)
-      super
-      base.extend const_get(:ClassMethods) if const_defined?(:ClassMethods)
-    end
-  end
+  mattr_accessor :case_insensitive_keys
+  @@case_insensitive_keys = [:devise_value]
+
+  mattr_accessor :hoge
+  @@case_insensitive_keys = [:hoge]
 end
 
 # https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models.rb#L15-L52
-module Models
-  def self.config(mod, *accessors)
-    # class << mod; attr_accessor :available_configs; end
-    def mod.available_configs
-      @available_configs
-    end
-    def mod.available_configs=(val)
-      @available_configs = val
-    end
-    mod.available_configs = accessors
-
-    accessors.each do |accessor|
-      mod.class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{accessor}
-          if defined?(@#{accessor})
-            @#{accessor}
-          elsif superclass.respond_to?(:#{accessor})
-            superclass.#{accessor}
-          else
-            Devise.#{accessor} # @TODO 対応
+module Devise
+  module Models
+    def self.config(mod, *accessors)
+      def mod.available_configs
+        @available_configs
+      end
+      def mod.available_configs=(val)
+        @available_configs = val
+      end
+      mod.available_configs = accessors
+  
+      accessors.each do |accessor|
+        mod.class_eval <<-METHOD, __FILE__, __LINE__ + 1
+          def #{accessor}
+            if defined?(@#{accessor})
+              @#{accessor}
+            elsif superclass.respond_to?(:#{accessor})
+              superclass.#{accessor}
+            else
+              Devise.#{accessor}
+            end
           end
-        end
-
-        def #{accessor}=(value)
-          @#{accessor} = value
-        end
-      METHOD
+  
+          def #{accessor}=(value)
+            @#{accessor} = value
+          end
+        METHOD
+      end
     end
   end
 end
@@ -57,14 +59,18 @@ module Authenticatable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    Models.config(self, :case_insensitive_keys)
+    Devise::Models.config(self, :request_keys, :case_insensitive_keys, :hoge)
   end
 end
 
 # https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models.rb#L88
 class User
-  extend ActiveSupport::Concern
   include Authenticatable
+  self.case_insensitive_keys = [:gmail]
+end
+
+class Qser < User
+  self.hoge = [:im_happy]
 end
 
 describe 'Authenticatable::ClassMethods' do
@@ -77,7 +83,7 @@ describe 'Authenticatable::ClassMethods' do
   end
 
   it 'は、インスタンス変数 @available_configs に値を持っている' do
-    _(Authenticatable::ClassMethods.instance_variable_get(:@available_configs)).must_equal %i(case_insensitive_keys)
+    _(Authenticatable::ClassMethods.instance_variable_get(:@available_configs)).must_equal %i(request_keys case_insensitive_keys hoge)
   end
 
 
@@ -103,6 +109,28 @@ describe 'User' do
   it 'は、パブリックメソッドに case_insensitive_keys= を持つ' do
     _(User.public_methods.include?(:case_insensitive_keys=)).must_equal true
   end
+
+  describe '#request_keys' do
+    it 'モデルクラスで設定しない場合Devise共通の値を取得することになる' do
+      _(User.request_keys).must_equal Devise.request_keys
+    end
+  end
+
+  describe '#case_insensitive_keys' do
+    it 'モデルクラスで設定する場合モデルクラス自体のインスタンス変数の値を取得することになる' do
+      _(User.case_insensitive_keys).must_equal [:gmail]
+    end
+  end
 end
 
-# @TODO User.case_insensitive_keys と User.case_insensitive_keys=(val) の動作確認
+describe Qser do
+  describe '#case_insensitive_keys' do
+    it 'モデルクラスで設定しない場合スーパークラス自体のインスタンス変数の値を取得することになる' do
+      _(Qser.case_insensitive_keys).must_equal User.case_insensitive_keys
+    end
+
+    it 'モデルクラスで設定する場合モデルクラス自体のインスタンス変数の値を取得することになる' do
+      _(Qser.hoge).must_equal [:im_happy]
+    end
+  end
+end
