724682acf028b8e63264da7dc0cadb87f945286b
soma <somofgit@gmail.com>
2024-12-07 10:29:58 +0900

    wip lib/devise/models/authenticatable.rb#downcase_keysを読みたい

--- ruby/exe.rb
+++ ruby/exe.rb
@@ -1,47 +1,69 @@
 require 'irb'
-require 'minitest/autorun'
+# require 'minitest/autorun'
 
+
+# ** 目標 **
+# 
+#  Deviseを読んでいる時にClassMethodsの中でクラスメソッドを呼んでいるコードに出会った。
+#  何をしているのか知りたいから読む。
+#  https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models/authenticatable.rb#L220-L223
+
+
+# ActiveSupportから必要な箇所を抜粋
+# https://github.com/rails/rails/blob/31aec233a17a44cb4666aa074243f0961798ed46/activesupport/lib/active_support/concern.rb#L125-L140
+module ActiveSupport
+  module Concern
+    def append_features(base)
+      super
+      base.extend const_get(:ClassMethods) if const_defined?(:ClassMethods)
+    end
+  end
+end
+
+# https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models.rb#L15-L52
+module Models
+  def self.config(mod, *accessors)
+    # class << mod; attr_accessor :available_configs; end
+    def mod.available_configs
+      @available_configs
+    end
+    def mod.available_configs=(val)
+      @available_configs = val
+    end
+
+    mod.available_configs = accessors
+
+    accessors.each do |accessor|
+      mod.class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{accessor}
+          if defined?(@#{accessor})
+            @#{accessor}
+          elsif superclass.respond_to?(:#{accessor})
+            superclass.#{accessor}
+          else
+            Devise.#{accessor} # @TODO 対応
+          end
+        end
+
+        def #{accessor}=(value)
+          @#{accessor} = value
+        end
+      METHOD
+    end
+  end
+end
+
+# https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models/authenticatable.rb#L220-L223
+module Authenticatable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    Models.config(self, :case_insensitive_keys)
+  end
+end
+
+# https://github.com/heartcombo/devise/blob/fec67f98f26fcd9a79072e4581b1bd40d0c7fa1d/lib/devise/models.rb#L88
 class User
-  attr_accessor :name, :age, :weight
-
-  def self.column_names
-    %w(name age weight)
-  end
-
-  def update(hash = {})
-    hash.keys.each{|k| send("#{k}=", hash[k])}
-  end
-end
-
-User.column_names.each do |column_name|
-  User.define_method("update_#{column_name}") do |value|
-    update(column_name => value)
-  end
-end
-
-class ModelReplicaTest < Minitest::Spec
-  it 'User.column_name' do
-    assert_equal %w(name age weight), User.column_names
-  end
-
-  it 'user.update' do
-    obj = User.new
-    obj.update(name: '太郎', age: 1000, weight: '60000kg')
-    assert_equal '太郎', obj.name
-    assert_equal 1000, obj.age
-    assert_equal '60000kg', obj.weight
-  end
-
-  it 'single column update' do
-    obj = User.new
-
-    obj.update_name '田中'
-    assert_equal '田中', obj.name
-
-    obj.update_age 50
-    assert_equal 50, obj.age
-
-    obj.update_weight '7000kg'
-    assert_equal '7000kg', obj.weight
-  end
+  extend ActiveSupport::Concern
+  include Authenticatable
 end
